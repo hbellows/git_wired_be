e4c978c7abfd10c61e971685f399a4c33fa47058
Harper Bellows <harper.bellows@gmail.com>
2019-02-13 14:40:44 -0700

    Add initial service call setup

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,2 +1,19 @@
 class GithubService
+
+  def location_data(login)
+    get_json("#{login}")
+  end
+
+  private
+
+  def conn
+    Faraday.new(url: '') do |faraday|
+      faraday.params['key'] = ENV['GITHUB_API_KEY']
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def get_json(url)
+    JSON.parse(conn.get(url).body, symbolize_names: true)
+  end
 end
