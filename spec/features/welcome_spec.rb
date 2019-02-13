2667375998fc65a14f8a98890774042078da0281
Harper Bellows <harper.bellows@gmail.com>
2019-02-13 12:43:50 -0700

    Add simplecov coverage directory to gitignore

--- spec/features/welcome_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe 'Visitor visits root page' do
-  it 'should display simple welcome message' do
-    visit '/'
-
-    expect(page).to have_content('Git Wired Endpoints')
-  end
-end
