350a3b4ccce64d468127e95ce635bdfc64efb530
Harper Bellows <harper.bellows@gmail.com>
2019-02-17 17:08:21 -0700

    Add repository project poro to customize json response object

--- app/poros/repo_project.rb
+++ app/poros/repo_project.rb
@@ -1,18 +1,22 @@
 class RepoProject
   
-  attr_reader :id,
+  attr_reader :repo_id,
+              :project_id, 
               :name, 
               :state,
               :creator,
               :creator_id,
+              :created_at,
               :updated_at
 
-  def initialize(data)
-    @id = data[:id]
+  def initialize(data, repo_id)
+    @repo_id = repo_id
+    @project_id = data[:id]
     @name = data[:name]
     @state = data[:state]
     @creator = data[:creator][:login]
     @creator_id = data[:creator][:id]
-    @updated_at = Time.parse(data[:updated_at]).strftime('%B %e, %Y')
+    @created_at = Time.parse(data[:created_at]).strftime('%B %d, %Y')
+    @updated_at = Time.parse(data[:updated_at]).strftime('%B %d, %Y')
   end
 end
