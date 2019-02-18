b76eefe73444d343197563d563a6c463ffba948f
Harper Bellows <harper.bellows@gmail.com>
2019-02-17 17:12:04 -0700

    Add passing request spec for GET to /api/v1/repositories/:id/projects route

--- spec/requests/api/v1/projects_request_spec.rb
+++ spec/requests/api/v1/projects_request_spec.rb
@@ -1,15 +1,18 @@
 require 'rails_helper'
 
-xdescribe 'user visits /api/v1/projects' do
+describe 'user visits /api/v1/projects' do
   context 'GET request' do
     it 'it returns a list of projects for a specific github user repository' do
-      # VCR.use_cassette('get_projects') do
-        user = User.create!(email: 'gracehopper@awesomesauce.com', user_name: 'grace_hopper', github_id: '12345', token: 'abc123')
-        repository = user.repositories.create!(name: 'My Repo', github_id: '76767676')
+      VCR.use_cassette('get_projects') do
+
+        user = User.create(email: 'harper.bellows@gmail.com', user_name: 'hbellows', github_id: '35637783', token: "#{ENV['GITHUB_API_KEY']}")
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
         
-        id = repository.id
-        
-        get "/api/v1/repositories/#{id}/projects"
+        repository = user.repositories.create!(name: 'git_wired_be', github_id: '170214553')
+      
+        repository_id = repository.id
+
+        get "/api/v1/repositories/#{repository_id}/projects"
 
         expect(response.status).to eq(200)
 
@@ -18,14 +21,17 @@ xdescribe 'user visits /api/v1/projects' do
         expect(returned_projects).to be_a(Hash)
         expect(returned_projects).to have_key(:data)
         expect(returned_projects[:data]).to have_key(:id)
-        expect(returned_projects[:data][:id]).to eq(user_name)
+        expect(returned_projects[:data][:id]).to eq(repository.name)
         expect(returned_projects[:data]).to have_key(:attributes)
-        expect(returned_projects[:data][:attributes]).to have_key(:projects)
-        # expect(returned_projects[:data][:attributes][:projects]).to be_a(Array)
-        # expect(returned_projects[:data][:attributes][:projects][0]).to have_key(:id)
-        # expect(returned_projects[:data][:attributes][:projects][0]).to have_key(:name)
-        # expect(returned_projects[:data][:attributes][:projects][0]).to have_key(:github_id)
-      # end
+        expect(returned_projects[:data][:attributes]).to have_key(:repository_projects)
+        expect(returned_projects[:data][:attributes][:repository_projects]).to be_a(Array)
+        expect(returned_projects[:data][:attributes][:repository_projects][0]).to have_key(:project_id)
+        expect(returned_projects[:data][:attributes][:repository_projects][0]).to have_key(:name)
+        expect(returned_projects[:data][:attributes][:repository_projects][0]).to have_key(:state)
+        expect(returned_projects[:data][:attributes][:repository_projects][0]).to have_key(:creator)
+        expect(returned_projects[:data][:attributes][:repository_projects][0]).to have_key(:created_at)
+        expect(returned_projects[:data][:attributes][:repository_projects][0]).to have_key(:updated_at)
+      end
     end
   end
 end
