5fef3d659e1ab0a5cfcee4b520a3ed1597e08463
Harper Bellows <harper.bellows@gmail.com>
2019-02-17 17:06:17 -0700

    Add working controller logic -> needs sad path and error handling in refactor round

--- app/controllers/api/v1/repositories/projects_controller.rb
+++ app/controllers/api/v1/repositories/projects_controller.rb
@@ -1,27 +1,16 @@
 class Api::V1::Repositories::ProjectsController < ApplicationController
 
   def index
-    render json: ProjectSerializer.new(repository.projects).serialized_json
+    render json: ProjectSerializer.new(project).serialized_json
   end
   
   private
 
   def repository
-    @repository ||= Repository.find_by(id: params[:repository_id])
+    @repository ||= current_user.repositories.find_by(id: params[:repository_id])
   end
-
+  
   def project
-    @project ||= ProjectFinder.new(params[:user_name], repository.id)
+    @project ||= ProjectFinder.new(current_user, repository)
   end
-  
-  # TODO: UPDATE THE ABOVE METHODS ONCE AUTHENTICATION IS FINALIZED
-
-  # def repository
-  #   @repository ||= current_user.repositories.find_by(id: params[:repository_id)
-  # end
-  
-  # def project
-  #   @project ||= ProjectFinder.new(current_user.user_name, repository)
-  # end
-
 end
