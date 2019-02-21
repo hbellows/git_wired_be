610bf574e91a1f46de87c2932e94e6a31bc6588e
Harper Bellows <harper.bellows@gmail.com>
2019-02-20 18:34:18 -0700

    Add route for wireframe index

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :repositories, module: :repositories, only: %i[index show] do
         resources :projects, only: %i[index show]
+        resources :wireframes, only: %i[index]
       end
     end	
   end
