188a9a9aa8726c7625238928e68144edd5503682
Jordan Whitten <jordanwa1947@gmail.com>
2019-02-20 21:32:40 -0700

    Add test for show wire frame enpoint

--- spec/requests/api/v1/wireframes_request_spec.rb
+++ spec/requests/api/v1/wireframes_request_spec.rb
@@ -7,18 +7,18 @@ describe 'GET requests' do
 
         user = User.create(email: 'harper.bellows@gmail.com', user_name: 'hbellows', github_id: '35637783', token: "#{ENV['GITHUB_API_KEY']}")
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-        
+
         repository = user.repositories.create!(name: 'git_wired_be', github_id: '170214553')
         wireframe_1 = repository.wireframes.create!(name: "Wireframe 1", object: "Description 1")
         wireframe_2 = repository.wireframes.create!(name: "Wireframe 2", object: "Description 2")
-        
+
         repository_id = repository.id
 
         get "/api/v1/repositories/#{repository_id}/wireframes"
         expect(response.status).to eq(200)
-        
+
         returned_wireframes = JSON.parse(response.body, symbolize_names: true)
-        
+
         expect(returned_wireframes).to be_a(Hash)
         expect(returned_wireframes).to have_key(:data)
         expect(returned_wireframes[:data]).to have_key(:id)
@@ -42,6 +42,40 @@ describe 'GET requests' do
         expect(returned_wireframes[:data][:attributes][:wireframes][1]).to have_key(:created_at)
         expect(returned_wireframes[:data][:attributes][:wireframes][1]).to have_key(:updated_at)
       # end
+    end
+  end
+
+  context 'user visits /api/v1/repositories/:id/wireframes/:id' do
+    it 'it returns the requested wireframe for a specific github user\'s repository' do
+
+        user = User.create(email: 'harper.bellows@gmail.com', user_name: 'hbellows', github_id: '35637783', token: "#{ENV['GITHUB_API_KEY']}")
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+        repository = user.repositories.create!(name: 'git_wired_be', github_id: '170214553')
+        wireframe_1 = repository.wireframes.create!(name: "Wireframe 1", object: "Description 1")
+        wireframe_2 = repository.wireframes.create!(name: "Wireframe 2", object: "Description 2")
+
+        repository_id = repository.id
+
+        get "/api/v1/repositories/#{repository_id}/wireframes/#{wireframe_2.id}"
+        expect(response.status).to eq(200)
+
+        returned_wireframe = JSON.parse(response.body, symbolize_names: true)
+
+        expect(returned_wireframe).to be_a(Hash)
+        expect(returned_wireframe).to have_key(:data)
+        expect(returned_wireframe[:data]).to have_key(:id)
+        expect(returned_wireframe[:data][:id]).to eq(repository.name)
+        expect(returned_wireframe[:data]).to have_key(:attributes)
+        expect(returned_wireframe[:data][:attributes]).to have_key(:wireframe)
+        expect(returned_wireframe[:data][:attributes][:wireframe]).to have_key(:id)
+        expect(returned_wireframe[:data][:attributes][:wireframe][:id]).to equal(wireframe_2.id)
+        expect(returned_wireframe[:data][:attributes][:wireframe]).to have_key(:name)
+        expect(returned_wireframe[:data][:attributes][:wireframe][:name]).to eq(wireframe_2.name)
+        expect(returned_wireframe[:data][:attributes][:wireframe]).to have_key(:object)
+        expect(returned_wireframe[:data][:attributes][:wireframe][:object]).to eq(wireframe_2.object)
+        expect(returned_wireframe[:data][:attributes][:wireframe]).to have_key(:created_at)
+        expect(returned_wireframe[:data][:attributes][:wireframe]).to have_key(:updated_at)
     end
   end
 end
